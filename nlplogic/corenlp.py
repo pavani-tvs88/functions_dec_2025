9ebb0a8ea26df98e81cd1d201d93c54e7777c025
pavani-tvs88 <76645064+pavani-tvs88@users.noreply.github.com>
2025-12-03 13:57:55 +0000

    Adding corenlp python

---
+++ nlplogic/corenlp.py
@@ -0,0 +1,24 @@
+from textblob import TextBlob
+import wikipedia
+
+def search_wikipedia(name, sentences=3):
+    """Search Wikipedia for a given name and return a summary."""
+    print(f"Searching Wikipedia for: {name}")
+    return wikipedia.search(name)
+
+def summarize_wikipedia(name, sentences=3):
+    """Get a summary from Wikipedia for a given name."""
+    print(f"Getting summary from Wikipedia for name: {name}")
+    return wikipedia.summary(name)
+
+def get_text_blob(text):
+    """Getting Text Blob object and returns"""
+    blob = TextBlob(text)
+    return blob
+
+def get_phrases(name):
+    """Find wikipedia name and return noun phrases."""
+    summary = summarize_wikipedia(name)
+    blob = get_text_blob(summary)
+    phrases = blob.noun_phrases
+    return phrases
